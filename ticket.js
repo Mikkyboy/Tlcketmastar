24bf651b1031090cdf6ef7bff4792fedfe31b856
Mikkyboy <michealoluwasina90@gmail.com>
2024-11-19 17:31:09 +0100

    update

---
+++ ticket.js
@@ -0,0 +1,25 @@
+const dropEl = document.getElementById('drop');
+
+const trandivEl = document.getElementById('trandiv');
+
+const btnEl1 = document.getElementById('btn1');
+
+const btnEl2 = document.getElementById('btn2');
+
+const btnEl3 = document.getElementById('btn3');
+
+const btnEl4 = document.getElementById('btn4');
+
+btnEl1.addEventListener('click', ()=> {
+  trandivEl.classList.add('show');
+  dropEl.classList.add('show');
+  console.log(trandivEl);
+})
+
+dropEl.addEventListener('click', ()=> {
+  trandivEl.classList.remove('show');
+  dropEl.classList.remove('show');
+})
+
+
+console.log('hey');
